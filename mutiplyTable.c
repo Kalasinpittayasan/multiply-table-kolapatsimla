497a1d86073ee4b48347dd314b588cb691b456e6
kolapatsimla <jkcbhappy@gmail.com>
2025-02-10 15:49:48 +0700

    Add files via upload

--- mutiplyTable.c
+++ mutiplyTable.c
@@ -3,6 +3,6 @@ int main() {
     int a;
     scanf("%d", &a);
     for(int i = 1; i <= 12; i++){
-        printf("%d x %d = %d\n", a, i, a * i);
+        printf("%d %d = %d\n", a, i, a * i);
     }
 }
